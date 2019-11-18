f5d0e7a0fafc6caa29779fe64808d203cf6646ad
ealiaj <ealiaj@uci.edu>
2019-11-17 16:05:15 -0800

    preprocessing data

---
+++ utils.py
@@ -0,0 +1,14 @@
+import torch
+
+
+def get_mean_and_std(dataset):
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True, num_workers=2)
+    mean = torch.zeros(3)
+    std = torch.zeros(3)
+    for inputs, targets in dataloader:
+        for i in range(3):
+            mean[i] += inputs[:, i, :, :].mean()
+            std[i] += inputs[:, i, :, :].std()
+    mean.div_(len(dataset))
+    std.div_(len(dataset))
+    return mean, std
