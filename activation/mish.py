f88453da6e14289a0965fcca70d55aab8d4596cf
ealiaj <ealiaj@uci.edu>
2019-12-06 14:35:13 -0800

    activation functions

---
+++ activation/mish.py
@@ -0,0 +1,41 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+def f_mish(input):
+    '''
+    Applies the mish function element-wise:
+    mish(x) = x * tanh(softplus(x)) = x * tanh(ln(1 + exp(x)))
+    '''
+    return input * torch.tanh(F.softplus(input))
+
+
+class mish(nn.Module):
+    '''
+    Applies the mish function element-wise:
+    mish(x) = x * tanh(softplus(x)) = x * tanh(ln(1 + exp(x)))
+
+    Shape:
+        - Input: (N, *) where * means, any number of additional
+          dimensions
+        - Output: (N, *), same shape as the input
+
+    Examples:
+        >>> m = mish()
+        >>> input = torch.randn(2)
+        >>> output = m(input)
+
+    '''
+
+    def __init__(self):
+        '''
+        Init method.
+        '''
+        super().__init__()
+
+    def forward(self, input):
+        '''
+        Forward pass of the function.
+        '''
+        return f_mish(input)
---
+++ activation/swish.py
@@ -0,0 +1,40 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+def f_swish(input):
+    '''
+    Applies the swish function element-wise:
+    swish(x) = x * sigmoid(x)
+    '''
+    return input * torch.sigmoid(input)
+
+
+class swish(nn.Module):
+    '''
+    Applies the swish function element-wise:
+    swish(x) = x * sigmoid(x)
+
+    Shape:
+        - Input: (N, *) where * means, any number of additional
+          dimensions
+        - Output: (N, *), same shape as the input
+
+    Examples:
+        >>> m = swish()
+        >>> input = torch.randn(2)
+        >>> output = m(input)
+
+    '''
+    def __init__(self):
+        '''
+        Init method.
+        '''
+        super().__init__()
+
+    def forward(self, input):
+        '''
+        Forward pass of the function.
+        '''
+        return f_swish(input)
